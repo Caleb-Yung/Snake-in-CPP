883a5734536436b14e1a4eb09378802a3ce6466c
Caleb-Yung <caleb.p.yung@gmail.com>
2024-12-28 22:43:29 -0500

    Update pop-up-mac.mm

--- pop-up-mac.m
+++ pop-up-mac.m
@@ -1,70 +1,33 @@
 #import <Cocoa/Cocoa.h>
+#include "snake.cpp" // Include your C++ file
 
-@interface PopupWindowController : NSWindowController
-@end
-
-@implementation PopupWindowController
-
-- (instancetype)init {
-    self = [super initWithWindow:nil];
-    if (self) {
-        NSWindow *popupWindow = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 300, 200)
-                                                            styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskClosable)
-                                                              backing:NSBackingStoreBuffered
-                                                                defer:NO];
-        popupWindow.title = @"Popup Window";
-        [popupWindow center];
-        self.window = popupWindow;
-
-        // Add a label to the popup window
-        NSTextField *label = [[NSTextField alloc] initWithFrame:NSMakeRect(50, 80, 200, 40)];
-        label.stringValue = @"This is a popup!";
-        label.bezeled = NO;
-        label.drawsBackground = NO;
-        label.editable = NO;
-        label.alignment = NSTextAlignmentCenter;
-        [popupWindow.contentView addSubview:label];
-    }
-    return self;
-}
-
-@end
-
+// Interface for the AppDelegate
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-@property (strong) NSWindow *mainWindow;
-@property (strong) PopupWindowController *popupController;
+@property (strong) NSWindow *window;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
-    // Create the main application window
-    self.mainWindow = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 400, 300)
-                                                  styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskResizable | NSWindowStyleMaskClosable)
-                                                    backing:NSBackingStoreBuffered
-                                                      defer:NO];
-    self.mainWindow.title = @"Main Window";
-    [self.mainWindow center];
-    [self.mainWindow makeKeyAndOrderFront:nil];
-
-    // Add a button to open the popup
-    NSButton *popupButton = [[NSButton alloc] initWithFrame:NSMakeRect(150, 130, 100, 30)];
-    popupButton.title = @"Show Popup";
-    popupButton.bezelStyle = NSBezelStyleRounded;
-    popupButton.target = self;
-    popupButton.action = @selector(showPopup);
-    [self.mainWindow.contentView addSubview:popupButton];
-}
-
-- (void)showPopup {
-    if (!self.popupController) {
-        self.popupController = [[PopupWindowController alloc] init];
-    }
-    [self.popupController showWindow:nil];
+    // Start the Snake game (call the startSnakeGame function from snake.cpp)
+    startSnakeGame();
 }
 
 @end
 
+// Replacement for MessageBox in C++ using NSAlert
+extern "C" void showGameOverMessage() {
+    @autoreleasepool {
+        NSAlert *alert = [[NSAlert alloc] init];
+        [alert setMessageText:@"Game Over"];
+        [alert setInformativeText:@"You hit the wall or your tail"];
+        [alert addButtonWithTitle:@"OK"];
+        [alert setAlertStyle:NSAlertStyleWarning];
+        [alert runModal]; // Display the alert
+    }
+}
+
+// Main function to set up the macOS application
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSApplication *app = [NSApplication sharedApplication];
